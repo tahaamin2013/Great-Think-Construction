4d458bc92485484537ca761f082a564aa06777a4
tahaamin2013 <tahaamindob2013@gmail.com>
2024-09-20 12:41:55 +0500

    fd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9571755808195636"
+          crossOrigin="anonymous"></script>
         <meta name='theme-color' content="#000" />
       </head>
       <body
