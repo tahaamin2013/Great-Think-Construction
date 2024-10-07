1b4dd4c97afce1d8ce392ccbdb6f27ed384591ec
tahaamin2013 <tahaamindob2013@gmail.com>
2024-10-08 00:06:48 +0500

    fds

---
+++ src/components/Google ads/Multiplexad.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useEffect } from 'react';
+
+export const Multiplex = () => {
+    useEffect(() => {
+        try {
+            // Load AdSense ads after the component mounts
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (error) {
+            console.error('Adsense error:', error);
+        }
+    }, []);
+
+    return (
+        <div>
+            <ins
+                className="adsbygoogle"
+                style={{ display: 'block' }}
+                data-full-width-responsive="true"
+                data-ad-format="autorelaxed"
+                data-ad-client="ca-pub-9571755808195636"
+                data-ad-slot="5006986571"
+            />
+        </div>
+    );
+}
