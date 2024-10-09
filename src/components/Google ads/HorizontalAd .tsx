82f43d1658fa96a89032fe6be950ead7e8432521
tahaamin2013 <tahaamindob2013@gmail.com>
2024-10-09 14:13:56 +0500

    add ad

--- src/components/Google ads/HorizontalAd .tsx
+++ src/components/Google ads/HorizontalAd .tsx
@@ -14,11 +14,11 @@ export const HorizontalAd = () => {
         <div className='flex w-full justify-center items-center'>
             <ins
                 className="adsbygoogle"
-                style={{ display: 'block', width: '100px', height: '100px' }}
-                data-full-width-responsive="true"
-                data-ad-format="auto"
+                style={{ display: 'block'}}
+                data-ad-format="fluid"
+                data-ad-layout-key="-gc+3e+8a-8g-bb"
                 data-ad-client="ca-pub-9571755808195636"
-                data-ad-slot="4717781342"
+                data-ad-slot="5773875974"
             />
         </div>
     );
