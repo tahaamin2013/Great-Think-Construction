c1c70c6cf4a0c0e1aa2b407cfe1a496b367873ef
tahaamin2013 <tahaamindob2013@gmail.com>
2024-10-09 14:06:38 +0500

    fd

--- src/components/Google ads/HorizontalAd .tsx
+++ src/components/Google ads/HorizontalAd .tsx
@@ -14,7 +14,7 @@ export const HorizontalAd = () => {
         <div className='flex w-full justify-center items-center'>
             <ins
                 className="adsbygoogle"
-                style={{ display: 'block', width: '100px', height: '100px' }} // Set your custom width and height here
+                style={{ display: 'block', width: '100px', height: '100px' }}
                 data-full-width-responsive="true"
                 data-ad-format="auto"
                 data-ad-client="ca-pub-9571755808195636"
