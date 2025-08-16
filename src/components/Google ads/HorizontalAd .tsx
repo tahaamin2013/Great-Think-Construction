5f0672689d9b1b6cf814cb5110245e6e074b652f
tahaamin2013 <tahaamindob2013@gmail.com>
2025-08-16 13:04:34 -0700

    fd

--- src/components/Google ads/HorizontalAd .tsx
+++ src/components/Google ads/HorizontalAd .tsx
@@ -15,9 +15,10 @@ export const HorizontalAd = () => {
             <ins
                 className="adsbygoogle"
                 style={{ display: 'block'}}
+                data-ad-format="fluid"
+                data-ad-layout-key="-gc+3e+8a-8g-bb"
                 data-ad-client="ca-pub-9571755808195636"
-                data-ad-slot="2173578983"
-                data-ad-format="auto"
+                data-ad-slot="5773875974"
             />
         </div>
         
