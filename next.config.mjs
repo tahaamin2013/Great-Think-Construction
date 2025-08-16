39e845cb26cb8fbfee36fed7353edf7b00f8cc01
tahaamin2013 <tahaamindob2013@gmail.com>
2025-08-16 13:16:07 -0700

    fd

--- next.config.mjs
+++ next.config.mjs
@@ -13,11 +13,7 @@ const nextConfig = {
             key: "X-Frame-Options",
             value: "SAMEORIGIN",
           },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-ancestors 'self';",
-          },
+  
           {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
