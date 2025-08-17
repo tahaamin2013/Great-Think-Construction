9ca4f61023125b7c2ded3788633f973cdaef724c
tahaamin2013 <tahaamindob2013@gmail.com>
2025-08-17 11:25:42 -0700

    fsd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { ProductUpdatesSection } from "@/components/product-updates-section"
 
 export default function BlogPage() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-[]">
       {/* <BlogHeader /> */}
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
