766d3f842143aea0d9ed95c07940cf787a3adce0
tahaamin2013 <tahaamindob2013@gmail.com>
2025-08-17 11:31:54 -0700

    fd

--- src/components/Google ads/Multiplexad.tsx
+++ src/components/Google ads/Multiplexad.tsx
@@ -2,25 +2,25 @@
 import { useEffect } from 'react';
 
 export const Multiplex = () => {
-    useEffect(() => {
-        try {
-            // Load AdSense ads after the component mounts
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        } catch (error) {
-            console.error('Adsense error:', error);
-        }
-    }, []);
+  useEffect(() => {
+    try {
+      // Load AdSense ads after the component mounts
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (error) {
+      console.error('Adsense error:', error);
+    }
+  }, []);
 
-    return (
-        <div className='px-auto mx-auto'>
-            <ins
-                className="adsbygoogle"
-                style={{ display: 'block' }}
-                data-full-width-responsive="true"
-                data-ad-format="autorelaxed"
-                data-ad-client="ca-pub-9571755808195636"
-                data-ad-slot="5006986571"
-            />
-        </div>
-    );
-}
+  return (
+    <div className="flex justify-center items-center w-full">
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block', margin: '0 auto' }}
+        data-full-width-responsive="true"
+        data-ad-format="autorelaxed"
+        data-ad-client="ca-pub-9571755808195636"
+        data-ad-slot="5006986571"
+      />
+    </div>
+  );
+};
